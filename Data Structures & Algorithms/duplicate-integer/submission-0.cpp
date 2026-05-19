bffa16d60dab0f5250556abd5c083e11f2477734
Ankush Bhardwaj <bhardwajankush493@gmail.com>
2026-05-19 12:40:52 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        map<int  , int>mpp;
+        for(auto x : nums){
+            if(mpp.find(x)!=mpp.end()){
+                return true;
+            }
+            mpp[x]++;
+        }
+        return false;
+    }
+};
